3ea055b45f4b53f3454e57075e05c85d15dac54c
seXy <matthew.louis@stu.spencer.kyschools.us>
2018-08-01 13:34:49 -0400

    just trollin around

--- resources/main.js
+++ resources/main.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
   var i = 0
   while (i < 100) {
       $('.mainbutton a').animate({'opacity':'.3'}, 1000);
-      $('.mainbutton a').animate({'opacity':'1'}, 1000);
+      $('.mainbutton a').animate({'opacity':'1'}, 1000).delay(100);
       i++;
     }
   $('.mainbutton a').mouseenter(function(){
-    var i = 100;
+    $('.mainbutton a').finish();
+    var i = 1000;
     $(this).css('color','red');
   });
 });
